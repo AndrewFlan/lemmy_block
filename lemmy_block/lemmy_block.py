478bfb32a7b3cff5e47d8b7f68e62b6c4fae9b80
Flanigan, Andrew <andrew.flann@gmail.com>
2023-08-15 16:07:13 -0500

    Issue #5 and skip already blocked

--- lemmy_block/lemmy_block.py
+++ lemmy_block/lemmy_block.py
@@ -84,12 +84,13 @@ def get_all_communities_from_instance(instance: str) -> list[BlockCommunity]:
                 run = False
             else:
                 for community in response.communities:
-                    block_list.append(
-                        BlockCommunity(
-                            name=community.community.name,
-                            instance=instance,
+                    if community.community.id is not None:
+                        block_list.append(
+                            BlockCommunity(
+                                name=community.community.name,
+                                instance=instance,
+                            )
                         )
-                    )
 
                 page += 1
                 sleep(0.25)
@@ -127,6 +128,46 @@ def get_block_list(config_file: Path) -> list[BlockCommunity]:
     return block_list
 
 
+def block_community(
+    index: int, total_count: int, community: object, instance: object
+) -> None:
+    """Function to preform logic for blocking a community
+
+    Args:
+        index (int): Index from Block List
+        total_count (int): Total number of communities from Block List
+        community (object): Community Object
+        instance (object): Instance Object
+    """
+    print(
+        f"({index + 1}/{total_count}) Blocking"
+        f" {community.name}@{community.instance} for"
+        f" {instance.account.user}"
+    )
+    try:
+        # Get the Community View
+        community_view: object = instance.get_community(
+            f"{community.name}@{community.instance}"
+        )
+        sleep(0.25)
+
+        # If community is blocked, skip
+        if community_view.blocked is True:
+            print(f"Skipping {community.name}@{community.instance}, already blocked")
+        # Block the community
+        else:
+            response = instance.block_community(
+                block=community.block,
+                community_id=(community_view.community.id),
+            )
+            print(f"Community {community.name} Blocked: {response}")
+            sleep(0.25)
+
+    except Exception as exception:  # pylint: disable=broad-except
+        print(f"Issue blocking {community.name}@{community.instance}")
+        print(f"Exception: {exception}")
+
+
 def block_communities(
     instances: list[Instance], block_list: list[BlockCommunity]
 ) -> None:
@@ -138,33 +179,50 @@ def block_communities(
     """
     total_count: int = len(block_list)
     print(f"Number of communities to block: {total_count}")
+
+    # Go through each instance defined
     for instance in instances:
         print(f"Attempting login for {instance.account.user}")
         result: bool = instance.login()
         sleep(0.25)
 
+        # If account login was successful, continue
         if result is True:
-            for index, community in enumerate(block_list):
-                try:
-                    print(
-                        f"({index + 1}/{total_count}) Blocking"
-                        f" {community.name}@{community.instance} for"
-                        f" {instance.account.user}"
-                    )
-                    response = instance.block_community(
-                        block=community.block,
-                        community_id=(
-                            instance.get_community(
-                                f"{community.name}@{community.instance}"
-                            )
-                        ),
-                    )
-                    print(f"Community {community.name} Blocked: {response}")
-                    sleep(0.25)
+            blocked_instances: list | None = None
 
-                except Exception as exception:  # pylint: disable=broad-except
-                    print("Issue blocking communities")
-                    print(f"Exception: {exception}")
+            # Get blocked instances from account's instance
+            try:
+                blocked_instances = instance.get_blocked_instances()
+                sleep(0.25)
+
+            except Exception as exception:  # pylint: disable=broad-except
+                print(f"Exception: {exception}")
+
+            # Go through each community and perform logic for blocking
+            for index, community in enumerate(block_list):
+                # If we failed to retrieve the Federated Instance List, can't skip anything
+                if blocked_instances is None:
+                    print(f"Failed to gather Blocked Instances for {instance.srv}")
+                    print("Will not be able to skip unnecessary blocks")
+
+                # If we retrieved the Federated Instance List, skip needed communities
+                if blocked_instances is not None:
+                    if any(i.domain == community.instance for i in blocked_instances):
+                        print(
+                            f"({index + 1}/{total_count}) "
+                            f"Skipping {community.name}@{community.instance},"
+                            f" instance {community.instance} blocked from"
+                            f" {instance.account.site}"
+                        )
+                        continue
+
+                # Block the community
+                block_community(
+                    index=index,
+                    total_count=total_count,
+                    community=community,
+                    instance=instance,
+                )
 
 
 def main():
@@ -172,20 +230,25 @@ def main():
 
     print("Starting lemmy_block.py")
 
+    # Gather ini files
     account_config_path = Path(os.path.dirname(__file__), "accounts.ini")
     check_config_file(account_config_path)
     block_config_path = Path(os.path.dirname(__file__), "blocklist.ini")
     check_config_file(block_config_path)
 
+    # Load account(s)
     accounts: list[Account] = get_accounts(account_config_path)
 
+    # Get instance(s)
     instances: list[Instance] = []
     for account in accounts:
         instance = Instance(account=account)
         instances.append(instance)
 
+    # Get the block list
     block_list: list = get_block_list(block_config_path)
 
+    # Perform Blocking Logic
     block_communities(instances, block_list)
 
     print("lemmy_block.py Complete!")
