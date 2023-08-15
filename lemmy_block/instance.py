c648ce5fb8ced99a4c4ae96f0ed290befafa90ed
Andrew Flanigan <andrew.flann@gmail.com>
2023-08-15 16:16:10 -0500

    lemmy_block  v1.0.1 (#8)
    * Issue #4 - Check if login successful or skip user (#6)
    
    * Skip account if failed login
    
    * Pipfile changes
    
    * Update exception handling
    
    * pylint
    
    * Issue #2 and #5 (#7)

--- lemmy_block/instance.py
+++ lemmy_block/instance.py
@@ -1,7 +1,11 @@
 """Account Class"""
 from account import Account
 from plemmy import LemmyHttp
-from plemmy.responses import GetCommunityResponse, BlockCommunityResponse
+from plemmy.responses import (
+    GetCommunityResponse,
+    BlockCommunityResponse,
+    GetFederatedInstancesResponse,
+)
 
 
 class Instance:
@@ -11,7 +15,7 @@ class Instance:
         self.account = account
         self.srv = LemmyHttp(self.account.site)
 
-    def login(self) -> None:
+    def login(self) -> bool:
         """Function to log into Lemmy account"""
         try:
             self.srv.login(self.account.user, self.account.password)
@@ -19,20 +23,24 @@ class Instance:
         except Exception as exception:  # pylint: disable=broad-except
             print(f"Unsuccessful login attempt for {self.account.user}")
             print(f"Exception: {exception}")
+            return False
 
-    def get_community(self, community_name: str) -> int | None:
+        print(f"Login successful for {self.account.user}")
+        return True
+
+    def get_community(self, community_name: str) -> object | None:
         """Function to get details about a community
 
         Args:
             community_name (str): Name of the community
 
         Returns:
-            int: ID of the community
+            object: Community View Object
         """
         try:
             api_response = self.srv.get_community(name=community_name)
             response = GetCommunityResponse(api_response)
-            return response.community_view.community.id
+            return response.community_view
 
         except Exception as exception:  # pylint: disable=broad-except
             print(f"Unsuccessful get community attempt for {community_name}")
@@ -65,3 +73,34 @@ class Instance:
             print(f"Exception: {exception}")
 
         return None
+
+    def get_blocked_instances(self) -> list | None:
+        """Function to get Blocked Instances from the current Instance
+
+        Returns:
+            list: List of Blocked Instances
+        """
+        blocked_instances: list = None
+        attempts: int = 0
+
+        while attempts <= 2:
+            print(
+                f"Attempt ({attempts + 1}/3) to pull Federated Instances List for"
+                f" {self.account.site}"
+            )
+            try:
+                api_response = self.srv.get_federated_instances()
+                res = GetFederatedInstancesResponse(api_response)
+
+            except Exception as exception:  # pylint: disable=broad-except
+                print(f"Couldn't get Federated Instances List. {exception}")
+                attempts += 1
+
+            else:
+                blocked_instances = res.federated_instances.blocked
+                break
+
+        if blocked_instances is None:
+            print("Failed to pull Federated Instances List")
+
+        return blocked_instances
